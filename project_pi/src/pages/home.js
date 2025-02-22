c70ee94c36e59fca9c4da782e1e664d513285924
qh89 <hq2139@nyu.edu>
2025-02-22 10:34:28 -0500

    frontend skeleton without calendar

--- project_pi/src/pages/home.js
+++ project_pi/src/pages/home.js
@@ -1,41 +1,14 @@
-import React, {useState} from "react";
-import DatePicker from 'react-datepicker';
+import React from "react";
+
 
 
 function Home(){
-    const [selectedDate, setSelectedDate] = useState(new Date());
+
     return(
         <div>
-            <h2>Select a Date</h2>
-            <DatePicker
-                selected={selectedDate}
-                onChange={(date) => setSelectedDate(date)}
-                dateFormat="yyyy-MM-dd"
-                className="custom-datepicker"
-            />
-            <p>Selected Date: {selectedDate.toDateString()}</p>
+            <p> Home</p>
         </div>
     )
 }
 
-const style = {
-    container: {
-        display: 'flex',
-        flexDirection: 'column',
-        alignItems: 'center',
-        justifyContent: 'center',
-        padding: '20px',
-        fontFamily: 'Arial, sans-serif',
-    },
-    heading: {
-        fontSize: '24px',
-        marginBottom: '20px',
-        color: '#202124',
-    },
-    selectedDate: {
-        marginTop: '20px',
-        fontSize: '16px',
-        color: '#5f6368',
-    },
-};
 export default Home;
